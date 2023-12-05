1e23d78ce361a3d2d413c9c667ecea18433395c4
elmfer <elmfer11@outlook.com>
2023-12-04 23:30:24 -0600

    added about me section component

---
+++ src/components/AboutMe.jsx
@@ -0,0 +1,18 @@
+function AboutMe() {
+  return (
+    <>
+      <h2 id="about-me"><i className="fa fa-circle-info"/> About Me</h2>
+      <p>
+          I am Fernando, a passionate programmer, who usually goes under the <strong>ELMFER</strong> pename. I have learned to code in Java, C++, Python, and recently HTML, CSS, and Javascript to create this website.
+      </p>
+      <p>
+          What I mostly do is Minecraft Modding. I had created and published a mod called Parkour Recorder on CurseForge that allows you to record your player's actions so that it can be played back at a later time. 
+      </p>
+      <p>
+          My mission is to create software to solve problems and entertain people through software. 
+      </p>
+    </>
+  )
+}
+
+export default AboutMe;
