e88cc5291476d5fbfe940a4e54fdc40fbf318c72
elmfer <elmfer11@outlook.com>
2023-12-04 23:33:32 -0600

    made projects page

---
+++ src/pages/ProjectsPage.jsx
@@ -0,0 +1,12 @@
+import ProjectList from "../components/ProjectList";
+
+function ProjectsPage() {
+  return (
+    <>
+      <p>Personal projects of mine. Some of them are also made with a team.</p>
+      <ProjectList />
+    </>
+  );
+}
+
+export default ProjectsPage;
