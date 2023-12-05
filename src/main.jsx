e120991e4aaf6c662b8d172f22c984cc777ea095
elmfer <elmfer11@outlook.com>
2023-12-04 23:28:55 -0600

    use page routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,44 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css'
 
+// Import fontawesome (for icons)
+import '../node_modules/@fortawesome/fontawesome-free/css/all.min.css'
+
+import HomePage from './pages/HomePage.jsx';
+import ErrorPage from './pages/ErrorPage.jsx';
+import AboutPage from './pages/AboutPage.jsx';
+import ProjectsPage from './pages/ProjectsPage.jsx';
+import ContactPage from './pages/ContactPage.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: 'about',
+        element: <AboutPage />,
+      },
+      {
+        path: 'projects',
+        element: <ProjectsPage />,
+      },
+      {
+        path: 'contact',
+        element: <ContactPage />,
+      },
+    ],
+  }
+]);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router} />,
 )
